c99f78e84aea6d90d2a0f39cb4d93db55f6a306e
Brian Lu <brian.lu62@gmail.com>
2024-01-15 18:27:06 -0600

    Connect front-end to back-end and performed API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,17 @@ import SearchPageComponent from './search-page/search-page'
 function App() {
   //Typical rule of thumb is to use the useState on the top to set variables
   //Fetching data from backend and logging it.
-  const [data, setData] = useState([{}])
-  useEffect(() => {
-    fetch("/homepage").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+  // const [data, setData] = useState([{}])
+  // useEffect(() => {
+  //   fetch("/homepage").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
   
 
 
@@ -22,14 +22,16 @@ function App() {
   
   return (
       <div>
-        {(typeof data.jsonfileexample === 'undefined') ? (
+        {
+          <SearchPageComponent></SearchPageComponent>
+        /* {(typeof data.jsonfileexample === 'undefined') ? (
           <p>Loading...</p>
         ) : (
           // <SearchPageComponent></SearchPageComponent>
           data.jsonfileexample.map(([testing], i) =>(
             <p key={i}>{testing}</p>
           ))
-        )}
+        )} */}
 
       </div>
     )
--- frontend/src/search-page/search-page.js
+++ frontend/src/search-page/search-page.js
@@ -1,37 +1,71 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import './search-page.css'
 
 export default function SearchPageComponent() {
-  const [searchInput, setSearchInput] = useState("");
-  const [searchResult, setSearchResult] = useState("");
+  const testUrl = '/getFullInfo';
 
-  const handleSearch = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState({});
+
+  function HandleSearch() {
     if(searchInput) {
-      setSearchResult(searchInput);
+      // Validates User Input to see if it is a valid IGN
+      let taglineIndex = searchInput.indexOf('#');
+      let _tagLine = searchInput.substring(taglineIndex + 1, searchInput.length);
+      if(taglineIndex < 0 || !_tagLine) {
+        setSearchResult({error: 'Please enter tagline'});
+        return;
+      }
+      let _gameName = searchInput.substring(0, taglineIndex);
+      if(!_gameName) {
+        setSearchResult('Please enter name');
+        return;
+      } 
+      setSearchResult('');
+
+      const urlParams = `gameName=${_gameName}&tagLine=${_tagLine}&server=na`;
+      const finalUrl = testUrl + '?' + urlParams;
+      const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      };
+
+      fetch(finalUrl, requestOptions)
+        .then(response => response.json())
+        .then(json => setSearchResult(json))
+        .catch(error => console.error(error));
     }
     else {
-      setSearchResult("Please provide a valid Summoner Name");
+      setSearchResult("Please provide a valid search");
     }
   }
 
   return (
     <div className='searchPageContainer'>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
+      <div className='searchForm'>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-      <input className='summonerSearchbar' type="text" placeholder="SummonerName#ID" onChange={event => {setSearchInput(event.target.value)}} 
-        onKeyDown={event => {if(event.key === 'Enter') handleSearch()} } value={searchInput}></input>
-      <p>{searchResult}</p>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">NA</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">EUW</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">EUN</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">OC</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">KR</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">JP</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">SG</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <input className='summonerSearchbar' type="text" placeholder="SummonerName#ID" onChange={event => {setSearchInput(event.target.value)}} 
+          onKeyDown={event => {if(event.key === 'Enter') HandleSearch()} } value={searchInput}></input>
+      </div>
+      <div className='searchResult'>
+        <p>{ JSON.stringify(searchResult) }</p>
+      </div>
     </div>
   );
 }
